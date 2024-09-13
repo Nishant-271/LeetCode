fc2b62785c1f3156b3581bef16932056a97325eb
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-13 11:41:21 +0530

    Time: 3 ms (10.5%), Space: 43.7 MB (10.95%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -1,34 +1,34 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        ListNode temp=head;
-        while(temp!=null){
-            map.put(temp.val,map.getOrDefault(temp.val,0)+1);
-            temp=temp.next;
-        }
-         ListNode head2 = null;
-         ListNode tail=null;
-         temp=head;
+         HashMap<Integer,Integer> map=new HashMap<>();   ListNode temp=head;
          while(temp!=null){
-           int value=temp.val;
-            int count=map.get(value);
-         
-          if(count==1){
-            ListNode nn=new ListNode(value);
-    
-          if(head2==null){
-            head2=nn;
-            tail=nn;
-          }
-          else{
-            tail.next=nn;
-            tail=nn;
-          }        
- 
-          }
-          temp=temp.next;
-         }
-return head2;
+            map.put(temp.val,map.getOrDefault(temp.val,0)+1);
+            temp=temp.next;     }
 
+ ListNode head2=null , tail=null; temp=head;
+ while(temp!=null){
+    int key=temp.val;   int count=map.get(key);
+
+  if(count==1){
+    ListNode nn=new ListNode(key);
+
+  if(head2==null){
+    head2=nn; tail=nn;
+  }
+  else{
+    tail.next=nn;
+    tail=nn;
+  }
+
+
+  }
+
+temp=temp.next;
+
+
+
+ }
+
+return head2;
     }
 }
