b652e732c09609471dd556bb8c9907fb7a4a50af
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-16 21:53:20 +0530

    Time: 0 ms (100%), Space: 41.5 MB (97.31%) - LeetHub

--- 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.java
@@ -1,31 +1,48 @@
 class Solution {
-    static int max=0;
-    public int maxAncestorDiff(TreeNode root) {
-        max=0;
-       find(root);
+//     static int max=0;
+//     public int maxAncestorDiff(TreeNode root) {
+//         max=0;
+//        find(root);
     
 
-return max;
+// return max;
+//     }
+
+//  public static void find(TreeNode root){
+//     if(root==null) return;
+//     findmax(root,root.left);
+//     findmax(root,root.right);
+
+//      find(root.left);
+//      find(root.right);
+
+//  }
+
+
+//  static void findmax(TreeNode root,TreeNode child){
+//     if(root==null || child==null) return;
+//     max=Math.max(max,Math.abs(root.val-child.val));
+
+//     findmax(root,child.left);
+//     findmax(root,child.right);
+//  }
+
+    public int maxAncestorDiff(TreeNode root) {
+     return max(root,root.val,root.val);
+     
+    
     }
 
- public static void find(TreeNode root){
-    if(root==null) return;
-    findmax(root,root.left);
-    findmax(root,root.right);
+    public static int max(TreeNode root,int ma,int mi){
+        if(root==null){
+            return Math.abs(ma-mi);
+        }
 
-     find(root.left);
-     find(root.right);
+  ma=Math.max(ma,root.val);
+  mi=Math.min(mi,root.val);
+   int l=max(root.left,ma,mi);
+   int r=max(root.right,ma,mi);
 
- }
-
-
- static void findmax(TreeNode root,TreeNode child){
-    if(root==null || child==null) return;
-    max=Math.max(max,Math.abs(root.val-child.val));
-
-    findmax(root,child.left);
-    findmax(root,child.right);
- }
-
-
+return Math.max(l,r);
+    }
 }
