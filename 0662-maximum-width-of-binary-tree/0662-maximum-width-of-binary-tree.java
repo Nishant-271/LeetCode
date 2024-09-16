e50c4ad055e28b6c5e67948f0fb1551aa7cd8b89
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-16 12:50:15 +0530

    Time: 1 ms (98.81%), Space: 43.1 MB (77.6%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -1,38 +1,33 @@
 class Solution {
-  class Pair{
-    TreeNode node;
-    int index;
+ class Pair{
+    TreeNode node; int index;
     Pair(TreeNode node,int index){
         this.node=node;
         this.index=index;
     }
-  }
-
+ }
 
     public int widthOfBinaryTree(TreeNode root) {
-       Queue<Pair> q=new LinkedList<>();
-       int maxwidth=0;
-       
-       q.add(new Pair(root,0));
- while(!q.isEmpty()){
-   int size=q.size();
-      int left=q.peek().index;
-      int right=left;
- for(int i=0;i<size;i++){
-    Pair current=q.remove();
-    TreeNode node=current.node;
-    int index=current.index;
-right=index;
- if(node.left!=null){
-    q.add(new Pair(node.left,2*index+1));
- }
- if(node.right!=null){
-    q.add(new Pair(node.right,2*index+2));
- }
- }
- maxwidth=Math.max(maxwidth,right-left+1);
- }
+      Queue<Pair> q=new LinkedList<>();
+      q.add(new Pair(root,0));
+      int maxwidth=0;
+      while(!q.isEmpty()){
+        int size=q.size();
+        int left=q.peek().index;
+        int right=left;
+        for(int i=0;i<size;i++){
+            Pair curr=q.remove();
+            TreeNode node=curr.node;
+            int index=curr.index;
+         right=index;
+         if(node.left!=null)   q.add(new Pair(node.left,2*index+1));
+         if(node.right!=null)   q.add(new Pair(node.right,2*index+2));
+          
+         
+        }
 
+        maxwidth=Math.max(maxwidth,right-left+1);
+      }
 return maxwidth;
     }
 }
