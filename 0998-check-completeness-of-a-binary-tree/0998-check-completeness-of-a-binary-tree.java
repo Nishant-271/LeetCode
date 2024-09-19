2d392744284accfa047cafb91914aa60d63d43f1
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-19 14:32:24 +0530

    Time: 0 ms (100%), Space: 41.7 MB (92.7%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
@@ -1,20 +1,42 @@
-
 class Solution {
+//  BY BFS ::
+
+//     public boolean isCompleteTree(TreeNode root) {
+//         if(root==null) return true;
+//     Queue<TreeNode> q=new LinkedList<>();
+//      boolean past=false;
+//     q.add(root);
+//           while(!q.isEmpty()){
+//             TreeNode k=q.remove();
+//             if(k==null){ past=true; }
+//             else{
+//                 if(past==true){ return false;}
+//             q.add(k.left); q.add(k.right);
+//             }
+//           }
+//   return true;
+//     }
+
+
+// BY DFS 
     public boolean isCompleteTree(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        boolean past=false;
-        while(!q.isEmpty()){
-      TreeNode k=q.remove();
-      if(k==null) past=true;
-      else{
-        if(past) return false;
-        else{
-            q.add(k.left);
-            q.add(k.right);
-        }
-      }
-        }
- return true;
+   int j=  countNodes(root);
+     int i=1;
+     return complete(root,i,j);
+    
     }
+
+    // count nodes
+    public static int countNodes(TreeNode root){
+        if(root==null) return 0;
+        return 1+countNodes(root.left)+countNodes(root.right);
+    }
+
+ public static boolean complete(TreeNode root,int i,int count){
+    if(root==null) return true;
+    if(i>count) return false;
+
+    return complete(root.left,2*i,count) && complete(root.right,2*i+1,count);
+ }
+
 }
