7f85ee0c5a46adb57dd29cf33ca5be82ef5e1a3b
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-21 12:01:17 +0530

    Time: 6 ms (5.29%), Space: 45.1 MB (8.13%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.java
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.java
@@ -1,35 +1,32 @@
 class Solution {
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-   List<Integer> del=new ArrayList<>();        
-   List<TreeNode> ans=new ArrayList<>();        
-
-  for(int i:to_delete){
-        del.add(i);  }
-if(!del.contains(root.val)){
-    ans.add(root);
-}
-
-
-    dfs(root,del,ans);
-
-    return ans;   
+        List<TreeNode> ans=new ArrayList<>();
+        List<Integer> del=new ArrayList<>();
+    for(int num:to_delete){
+        del.add(num);
     }
-  public static TreeNode dfs(TreeNode root,List<Integer>del,List<TreeNode>ans){
+   func(root,del,ans);
+    if(!del.contains(root.val)){
+        ans.add(root);
+    }
+   return ans;
+    }
+
+public static TreeNode func(TreeNode root,List<Integer> del,List<TreeNode> ans){
     if(root==null) return null;
 
-    root.left=dfs(root.left,del,ans);
-    root.right=dfs(root.right,del,ans);
- if(del.contains(root.val)){
-    if(root.left!=null) ans.add(root.left);
-    if(root.right!=null) ans.add(root.right);
- 
- return null;
- }
+    root.left=func(root.left,del,ans);
+    root.right=func(root.right,del,ans);
 
-
-return root;
+  if(del.contains(root.val)){ // agar aisa hai toh node delete krni hai 
+    if(root.left!=null) ans.add(root.left); // left node as new root add to ans list
+    if(root.right!=null) ans.add(root.right); //right node as new root add to ans list
+return null;
   }
 
+ return root;
 
+
+}
 
 }
