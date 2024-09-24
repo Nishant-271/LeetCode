bdb81e0220a1cbbac52f6ac4585d64dca710b4ba
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-24 10:42:55 +0530

    Time: 0 ms (100%), Space: 40.9 MB (79.16%) - LeetHub

--- 0832-binary-tree-pruning/0832-binary-tree-pruning.java
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.java
@@ -1,19 +1,27 @@
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-  if(root==null) return null;
-  if(!isonepresent(root.left)) root.left=null;
-  if(!isonepresent(root.right)) root.right=null;
-  pruneTree(root.left);
-  pruneTree(root.right);
+ if(root==null) return null;
+
+ if(!onepresent(root.left)){
+    root.left=null;
+ }
+if(!onepresent(root.right)){
+    root.right=null;
+}
+
+pruneTree(root.left);
+pruneTree(root.right);
 
 if(root.left==null && root.right==null && root.val==0) return null;
-return root;
+
+ 
+ return root;
     }
-public static boolean isonepresent(TreeNode root){
+public static boolean onepresent(TreeNode root){
     if(root==null) return false;
     if(root.val==1) return true;
+    return onepresent(root.left) || onepresent(root.right);
 
-    return isonepresent(root.left) || isonepresent(root.right);
 }
 
 
