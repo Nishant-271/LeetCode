a9ad3fb3652d88319eb8383308f598e82bf756ac
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-24 11:55:32 +0530

    Time: 1 ms (99.98%), Space: 45 MB (17.72%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-  List<List<Integer>> result=new ArrayList<>();
-  List<Integer> current=new ArrayList<>();
-int sum=0;
+   List<List<Integer>> result=new ArrayList<>();
+   List<Integer> current=new ArrayList<>();
+  int sum=0;
  solve(root,targetSum,result,current,sum);
-return result;
+
+  return result;
+ 
+
     }
 
 public static void solve(TreeNode root,int target,List<List<Integer>> result,List<Integer>current,int sum){
-    if(root==null) return;
+    if(root==null) return ;
     sum+=root.val;
     current.add(root.val);
-    // leaf node
 
-if(root.left==null && root.right==null){
+ if(root.left==null && root.right==null){
     if(sum==target){
         result.add(new ArrayList<>(current));
     }
-}
+ }
 
-solve(root.left,target,result,current,sum);
-solve(root.right,target,result,current,sum);
+ solve(root.left,target,result,current,sum);
+ solve(root.right,target,result,current,sum);
+ current.remove(current.size()-1);
 
-current.remove(current.size()-1);
-}
+} 
 
 }
