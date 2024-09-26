14097bf50eee84841f2ba628695e408f91b40590
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-26 13:07:20 +0530

    Time: 7 ms (17.34%), Space: 45.1 MB (13.26%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,38 +1,40 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-       HashMap<Integer,List<Integer>> map=new HashMap<>();
-       for(int edge[]:edges){
+    HashMap<Integer,List<Integer>> map=new HashMap<>();
+    for(int edge[]:edges){
         int u=edge[0];
         int v=edge[1];
+    //    for u-->v
         if(!map.containsKey(u)){
             map.put(u,new ArrayList<>());
         }
         map.get(u).add(v);
-        
-        if(!map.containsKey(v)){
-            map.put(v,new ArrayList<>());
-        }
-        map.get(v).add(u);
-
-       boolean visited[]=new boolean[edges.length+1];
-         if(dfs(map,u,v,visited)){
-            return edge;
-         }
-
-       }
+    // for v-->u
+    if(!map.containsKey(v)){
+        map.put(v,new ArrayList<>());
+    }
+    map.get(v).add(u);
+    
+    boolean visited[]=new boolean[edges.length+1];
+    // function call
+    if(dfs(map,u,v,visited)){
+        return edge;
+    }
+    }     
 
 return new int[0];
     }
-public static boolean dfs(HashMap<Integer,List<Integer>>map,int start,int parent,boolean visited[]){
+
+public static boolean dfs(HashMap<Integer,List<Integer>> map,int start,int parent,boolean visited[]){
     visited[start]=true;
     if(map.containsKey(start)){
         for(int nn:map.get(start)){
             if(nn==parent) continue;
             if(visited[nn]) return true;
-            if(dfs(map,nn,start,visited)) return true;
+            if(dfs(map,nn,start,visited))  return true;
         }
     }
-return false;
+    return false;
 }
 
 }
