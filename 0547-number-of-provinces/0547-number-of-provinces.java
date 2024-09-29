04d3c7d18b14700848462f5074a9ed488a49c82e
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-29 16:03:57 +0530

    Time: 1 ms (91.11%), Space: 47.8 MB (36.99%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,43 +2,29 @@ class Solution {
     public int findCircleNum(int[][] mat) {
         // By hashmap using bfs 
      int n=mat.length;
-     HashMap<Integer,List<Integer>>map=new HashMap<>();
-     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(mat[i][j]==1){
-                 if(!map.containsKey(i))
-                    map.put(i,new ArrayList<>());
-                map.get(i).add(j);
-              
-                 if(!map.containsKey(j)) map.put(j,new ArrayList<>());
-                map.get(j).add(i);
-            }
-        }
-}
   int count=0;
   boolean visited[]=new boolean[n];
   for(int u=0;u<n;u++){
     if(!visited[u]){
-        bfs(map,visited,u);
+        bfs(mat,visited,u);
         count++;
     }
   }
 
 return count;
     }
-public static void bfs(HashMap<Integer,List<Integer>> map,boolean visited[],int u){
+
+public static void bfs(int [][]mat,boolean visited[],int u){
     Queue<Integer> q=new LinkedList<>();
     q.add(u);
     visited[u]=true;
      while(!q.isEmpty()){
      int node=q.remove();
-    
-    if(map.containsKey(node)){
-        for(int v:map.get(node)){
-            if(!visited[v])
-           bfs(map,visited,v);
-        }
-    }}
+  for(int v=0;v<mat.length;v++){
+     if(!visited[v] && mat[node][v]==1){
+        bfs(mat,visited,v);
+     }
+  }
 }
-
+}
 }
