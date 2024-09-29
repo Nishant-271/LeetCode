1be80bc2c18771c54034682e5cd754d5597b4784
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-29 15:59:42 +0530

    Time: 8 ms (6.3%), Space: 46.8 MB (85.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,45 +1,44 @@
 class Solution {
-    public int findCircleNum(int[][] edges) {
-       int n=edges.length;
-    // by without hashmap
-   
-      /* 
-//   by hashmap 
-        HashMap<Integer,List<Integer>> map=new HashMap<>();
-       for(int i=0;i<n;i++){
-           for(int j=0;j<n;j++){
-            if(edges[i][j]==1)
-            
-            {if(!map.containsKey(i)){
-                map.put(i,new ArrayList<>());
+    public int findCircleNum(int[][] mat) {
+        // By hashmap using bfs 
+     int n=mat.length;
+     HashMap<Integer,List<Integer>>map=new HashMap<>();
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(mat[i][j]==1){
+                 if(!map.containsKey(i))
+                    map.put(i,new ArrayList<>());
+                map.get(i).add(j);
+              
+                 if(!map.containsKey(j)) map.put(j,new ArrayList<>());
+                map.get(j).add(i);
             }
-            map.get(i).add(j);
-
-            if(!map.containsKey(j)){
-                map.put(j,new ArrayList<>());
-            }
-            map.get(j).add(i);
-           }
-        }}
-        */
-   int count=0;
-   boolean visited[]=new boolean[n];
-   for(int u=0;u<n;u++){
+        }
+}
+  int count=0;
+  boolean visited[]=new boolean[n];
+  for(int u=0;u<n;u++){
     if(!visited[u]){
-        dfs(edges,visited,u);
+        bfs(map,visited,u);
         count++;
     }
-   }
+  }
 
 return count;
     }
+public static void bfs(HashMap<Integer,List<Integer>> map,boolean visited[],int u){
+    Queue<Integer> q=new LinkedList<>();
+    q.add(u);
+    visited[u]=true;
+     while(!q.isEmpty()){
+     int node=q.remove();
+    
+    if(map.containsKey(node)){
+        for(int v:map.get(node)){
+            if(!visited[v])
+           bfs(map,visited,v);
+        }
+    }}
+}
 
-    public static void dfs(int[][]edges,boolean visited[],int u){
-        visited[u]=true;
-       for(int v=0;v<edges.length;v++){
-           if(!visited[v] && edges[u][v]==1){
-            dfs(edges,visited,v);
-           }
-       }
-    }
 }
