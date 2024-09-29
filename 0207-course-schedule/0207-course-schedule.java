b796242dbd1d92a2aa8ad3afe1abecdc932db868
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-29 22:54:50 +0530

    Time: 6 ms (52.81%), Space: 45.9 MB (16.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,51 +1,43 @@
 class Solution {
     public boolean canFinish(int V, int[][] edges) {
-        
-        /* Approch topological sort jaise concept hai 
-        directed graph me cycle dekh lenge agar cycle hogi toh return false else return true;
-*/
-HashMap<Integer,List<Integer>> map=new HashMap<>();
-        boolean visited[]=new boolean[V];
-        boolean current[]=new boolean[V];
-       for(int []edge:edges){
-        int end=edge[0];
-        int start=edge[1];
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        for(int []edge:edges){
+           int end=edge[0];
+           int start=edge[1];
         if(!map.containsKey(start)){
             map.put(start,new ArrayList<>());
         }
         map.get(start).add(end);
-       }
-
- for(int u=0;u<V;u++){
-      if(!visited[u]){
-        if(dfs(map,visited,current,u)){
-            return false;
         }
-      }
-      
- }
+        boolean [] visited=new boolean[V];
+        boolean [] current=new boolean[V];
+   for(int u=0;u<V;u++){
+    if(!visited[u]){
+        if(dfs(map,visited,current,u)){
+return false;
+        }
+    }
+   }
 
 return true;
     }
-public static boolean dfs(HashMap<Integer,List<Integer>>map,boolean visited[],boolean current[],int node){
-    visited[node]=true;
-     current[node]=true;
- if(map.containsKey(node)){
-   for(int nn:map.get(node)){
-    if(!visited[nn]){
-        if(dfs(map,visited,current,nn)) return true;
-    }
- else{
-    if(visited[nn]==true &&  current[nn]==true){
-        return true;
-    }
- }
-   }
- }
 
- current[node]=false;
- return false;
-
+public static boolean dfs(HashMap<Integer,List<Integer>> map,boolean visited[],boolean current[],int start){
+    visited[start]=true;
+    current[start]=true;
+  if(map.containsKey(start)){
+    for(int nn:map.get(start)){
+        if(!visited[nn]){
+            if(dfs(map,visited,current,nn)) return true;
+        }
+        else{
+            if(current[nn]==true){
+                return true;
+            }
+        }
+    }
+  }
+  current[start]=false;
+return false;
 }
-
 }
