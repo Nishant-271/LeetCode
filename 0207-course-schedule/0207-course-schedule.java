57e381a49c029fb3049cb9028ae93d73d3fa3961
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-30 15:36:48 +0530

    Time: 9 ms (19.49%), Space: 45.8 MB (20.7%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean canFinish(int V, int[][] edges) {
         HashMap<Integer,List<Integer>> map=new HashMap<>();
+    int indegree[]=new int[V];
         for(int []edge:edges){
            int end=edge[0];
            int start=edge[1];
@@ -8,36 +9,34 @@ class Solution {
             map.put(start,new ArrayList<>());
         }
         map.get(start).add(end);
-        }
-        boolean [] visited=new boolean[V];
-        boolean [] current=new boolean[V];
-   for(int u=0;u<V;u++){
-    if(!visited[u]){
-        if(dfs(map,visited,current,u)){
-return false;
-        }
-    }
-   }
+        indegree[end]++;
+     }
 
-return true;
+    Queue<Integer> q=new LinkedList<>();
+  int count=0;
+ for(int u=0;u<V;u++){
+    if(indegree[u]==0){
+        q.add(u);
+        count++;
     }
+ }
 
-public static boolean dfs(HashMap<Integer,List<Integer>> map,boolean visited[],boolean current[],int start){
-    visited[start]=true;
-    current[start]=true;
-  if(map.containsKey(start)){
-    for(int nn:map.get(start)){
-        if(!visited[nn]){
-            if(dfs(map,visited,current,nn)) return true;
+  
+while(!q.isEmpty()){
+    int u=q.remove();
+    if(map.containsKey(u)){
+    for(int v:map.get(u)){
+        indegree[v]--;
+        if(indegree[v]==0){
+            q.add(v);
+            count++;
         }
-        else{
-            if(current[nn]==true){
-                return true;
-            }
-        }
-    }
-  }
-  current[start]=false;
-return false;
+    }}
 }
+ 
+ if(count==V) return true;
+ return false;
+ 
+    }
+
 }
