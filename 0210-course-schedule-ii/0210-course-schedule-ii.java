5d0db859ac65c30c448c0cbce38c35c01ff32929
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-09-30 16:06:46 +0530

    Time: 7 ms (40.35%), Space: 45.4 MB (85.96%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,54 +1,46 @@
 class Solution {
     public int[] findOrder(int V, int[][] edges) {
-//  inputs ::
-        boolean visited[]=new boolean[V]; 
-        boolean current[]=new boolean[V]; 
-       HashMap<Integer,List<Integer>> map=new HashMap<>();
-       Stack<Integer> st=new Stack<>();
- // making of graph:
-for(int []edge:edges){
-    int end=edge[0];
-    int start=edge[1];
- if(!map.containsKey(start)){
-    map.put(start,new ArrayList<>());
- }
-map.get(start).add(end);
-}
-
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+      int [] indegree=new int[V];
+      int result[]=new int[V];
+    //   GRAPH MAKING AND INDEGREE ARRAY 
+      for(int edge[] : edges){
+        int end=edge[0];
+        int start=edge[1];
+       if(!map.containsKey(start)){
+        map.put(start,new LinkedList<>());
+       }
+       map.get(start).add(end);
+       indegree[end]++;
+      }
+ 
+ Queue<Integer> q=new LinkedList<>();
+ int count=0; int i=0;
  for(int u=0;u<V;u++){
-    if(!visited[u]){
-        if(dfs(map,visited,current,u,st)){ 
-           return new int[0];
-        }
+    if(indegree[u]==0){
+        count++;
+        q.add(u);
+        result[i++]=u;
     }
  }
 
- int result[]=new int[st.size()];
- int i=0;
- while(!st.isEmpty()){
-    result[i++]=st.pop();
- }
-
-return result;
-
-    }
-
-public static boolean dfs(HashMap<Integer,List<Integer>> map,boolean visited[],boolean current[],int u,Stack<Integer> st){
-    visited[u]=true;
-    current[u]=true;
+while(!q.isEmpty()){
+    int u=q.remove();
     if(map.containsKey(u)){
         for(int v:map.get(u)){
-            if(!visited[v]){
-                if(dfs(map,visited,current,v,st)) return true;
-            }
-            else{
-                if(visited[v]==true && current[v]==true) return true;
+            indegree[v]--;
+            if(indegree[v]==0){
+                count++;
+                q.add(v);
+                result[i++]=v;
             }
         }
     }
-st.push(u);
-current[u]=false;
-return false;
 }
-
+ 
+ if(count==V){
+    return result;
+ }
+ return new int[0];
+    }
 }
