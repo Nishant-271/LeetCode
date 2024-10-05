b07896943b658b2c1c9fbda19d7faef6c3ad62ca
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-05 16:04:06 +0530

    Time: 1 ms (63.23%), Space: 44.2 MB (99.52%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,43 +1,33 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
- int V=graph.length;
- int currentcolor=1;
-  int color[]=new int[V];
- for(int i=0;i<V;i++){
-    color[i]=-1;
- }
-
-  HashMap<Integer,List<Integer>> map=new HashMap<>();
-  for(int u=0;u<V;u++){
-    List<Integer> neighbor=new ArrayList<>();
-    for(int v=0;v<graph[u].length;v++){
-        neighbor.add(graph[u][v]);
+        int V=graph.length;
+        int color[]=new int[V];
+        for(int i=0;i<V;i++){
+            color[i]=-1;
+        }
+        int currentcolor=1;
+        for(int u=0;u<V;u++){
+            if(color[u]==-1){
+                if(bfs(graph,u,color,currentcolor)==false) return false;
+            }
+        }
+    return true;
     }
-    map.put(u,neighbor);
-  }
-
-
- for(int u=0;u<V;u++){
-    if(color[u]==-1){
-        if(dfs(map,u,color,currentcolor)==false) return false;
-    }
- }
- return true;
-    }
-public static boolean dfs(HashMap<Integer,List<Integer>> map,int u,int color[],int currentcolor){
-    color[u]=currentcolor;
-    if(map.containsKey(u)){
-        for(int v:map.get(u)){
-            if(color[v]==color[u]) return false;
-            if(color[v]==-1){
-                int color_of_v=1-color[u];
-                if(dfs(map,v,color,color_of_v)==false){
-                    return false;
+    public static boolean bfs(int[][]graph,int node,int[]color,int currentcolor){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(node);
+        color[node]=currentcolor;
+        while(!q.isEmpty()){
+            int u=q.remove();
+            for(int v:graph[u]){
+                if(color[v]==color[u]) return false;
+                if(color[v]==-1){
+                    // usko diffrent color deke q me daal dunga 
+                    color[v]=1-color[u];
+                    q.add(v);
                 }
             }
         }
+ return true;
     }
-return true;
-}
-
 }
