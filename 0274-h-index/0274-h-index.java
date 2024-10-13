d6794184b970fe2c2d1e41d0470f67eb4ea282eb
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-13 23:07:19 +0530

    Time: 2 ms (72.41%), Space: 41.6 MB (30.62%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,14 +1,3 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int n=citations.length;
-        int hin=0;
-        for(int i=0;i<n;i++){
-            int h=n-i;
-            if(citations[i]>=h){
-                hin=h;break;
-            }
-        }
-return hin;
-    }
-}
+        Arrays.sort(citations);int n=citations.length;int hin=0;for(int i=0;i<n;i++){ int h=n-i;if(citations[i]>=h){hin=h;break;}}return hin;}}
