ca400f78cd8f747f50b6eb9549bc2a6180a19260
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-15 11:49:52 +0530

    Time: 0 ms (100%), Space: 56.8 MB (91.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,7 +3,7 @@ class Solution {
     //  7654321 -> 5674321  -> 5671234
 
 
-        if(k>nums.length) k=k%nums.length;
+        if(k>=nums.length) k=k%nums.length;
       reverse(0,nums.length-1,nums,k);
       reverse(0,k-1,nums,k);
       reverse(k,nums.length-1,nums,k);
