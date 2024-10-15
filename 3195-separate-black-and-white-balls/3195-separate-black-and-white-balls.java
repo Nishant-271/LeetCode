ba7b9769ac8830876962fa193080ae7763d550fb
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-15 14:54:22 +0530

    Time: 9 ms (22.22%), Space: 45.3 MB (54.12%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -3,14 +3,12 @@ class Solution {
         long count=0;
         long finalcount=0;
        for(int i=s.length()-1;i>0;i--){
-        int j=i;
-        if(s.charAt(i)=='0') count++;
          if(s.charAt(i)=='0' ){
-          while(j>0 && s.charAt(j-1)=='1'){       
+            count++;
+          while(i>0 && s.charAt(i-1)=='1'){       
                finalcount+=count;
-               j--;
+               i--;
        }}
-       i=j;
        }
        return finalcount;
     }
