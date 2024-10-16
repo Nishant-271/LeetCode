400ce817cd1965852c83bc7cd5a11fab056783b9
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-16 14:57:58 +0530

    Time: 346 ms (5.02%), Space: 45.4 MB (22.15%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        for(int i=1;i<=n;i++){
+         boolean flag=false;
+         for(int j=0;j<nums.length;j++){
+               if(nums[j]==i){
+                flag=true;
+
+               } 
+         }
+         if(flag==false){
+            return i;
+         }
+        }
+        return 0;
+    }
+}
