bac837ba13662bfc5dfcb972009d56fa0aef4001
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-21 10:29:47 +0530

    Time: 1 ms (99.82%), Space: 46.1 MB (47.91%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
         // BRUTE FORCE ::
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]==nums[i]){
-                    count++;
-                }
-            }
-        if(count==1) return nums[i];
-        }
-return -1;
+//         for(int i=0;i<nums.length;i++){
+//             int count=0;
+//             for(int j=0;j<nums.length;j++){
+//                 if(nums[j]==nums[i]){
+//                     count++;
+//                 }
+//             }
+//         if(count==1) return nums[i];
+//         }
+// return -1;
 
+
+ // BY OPTIMAL SOLUTION ::
+ int xor=0;
+ for(int i:nums){
+    xor^=i;
+ }
+return xor;
 
     }
 }
