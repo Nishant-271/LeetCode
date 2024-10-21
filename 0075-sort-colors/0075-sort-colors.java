4d6f3b96d8a24810edd051bb8b88d4e53c571577
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-21 10:46:47 +0530

    Time: 0 ms (100%), Space: 41.7 MB (66.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,15 +4,35 @@ class Solution {
         // Arrays.sort(nums);
 
 //  BYUSING TWO FOR LOOPS SWAPPING 
-for(int i=0;i<nums.length;i++){
-    for(int j=i+1;j<nums.length;j++){
-        if(nums[i]>nums[j]){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-        }
+// for(int i=0;i<nums.length;i++){
+//     for(int j=i+1;j<nums.length;j++){
+//         if(nums[i]>nums[j]){
+//             int temp=nums[i];
+//             nums[i]=nums[j];
+//             nums[j]=temp;
+//         }
+//     }
+// }
+
+//  BY USING DUTCH NATIONAL FLAG ALGORITHM ::
+int mid=0,low=0,high=nums.length-1;
+while(mid<=high){
+    if(nums[mid]==0){
+        int temp=nums[mid];
+        nums[mid]=nums[low];
+        nums[low]=temp;
+        mid++;
+        low++;
+    }
+    else if(nums[mid]==1) mid++;
+    else{
+        int temp=nums[mid];
+        nums[mid]=nums[high];
+        nums[high]=temp;
+        high--;
     }
 }
+
 
     }
 }
