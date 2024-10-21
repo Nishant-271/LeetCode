52c3667619cbf0a9affd33cd8d0e29bc17db0098
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-21 10:08:43 +0530

    Time: 3 ms (24.62%), Space: 45.7 MB (66.19%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,38 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         // ONE METHOD ::
-        int index=0;
-        for(int num:nums){
-            if(num!=0){
-                nums[index++]=num;
-            }
-        }
+        // int index=0;
+        // for(int num:nums){
+        //     if(num!=0){
+        //         nums[index++]=num;
+        //     }
+        // }
 
-        while(index<nums.length){
-            nums[index++]=0;
+        // while(index<nums.length){
+        //     nums[index++]=0;
+        // }
+    
+    // TWO SWAP METHOD J KO FIRST ZERO PR POHOCHAU AND THEN AGAR NEXT 
+    // ELEMENT ZERO NHI HAI TOH USSE SWAP KRTE JAO 
+     
+     int j=-1;
+     for(int i=0;i<nums.length;i++){
+        if(nums[i]==0){
+            j=i;
+            break;
         }
+     }
+     if(j==-1) return;
+
+   for(int i=j+1;i<nums.length;i++){
+    if(nums[i]!=0){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        j++;
+    }
+   }
+
+
     }
 }
