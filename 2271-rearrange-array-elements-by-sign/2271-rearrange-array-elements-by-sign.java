7362b2543e673f48c95ce05f4decd618c742d1da
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-22 13:56:13 +0530

    Time: 3 ms (100%), Space: 76.6 MB (81.1%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -3,24 +3,40 @@ class Solution {
        
     //    BRUTE FORCE ::::
        
-        int plus[] =new int[nums.length/2];
-        int minus[] =new int[nums.length/2];
-       int p=0; int m=0;
-     for(int i=0;i<nums.length;i++){
-        if(nums[i]>0){
-            plus[p++]=nums[i];
-        }
-        else{
-            minus[m++]=nums[i];
-        }
-    }
-      for(int i=0;i<nums.length/2;i++){
-        nums[2*i]=plus[i];
-        nums[(2*i)+1]=minus[i];
-      }
+//         int plus[] =new int[nums.length/2];
+//         int minus[] =new int[nums.length/2];
+//        int p=0; int m=0;
+//      for(int i=0;i<nums.length;i++){
+//         if(nums[i]>0){
+//             plus[p++]=nums[i];
+//         }
+//         else{
+//             minus[m++]=nums[i];
+//         }
+//     }
+//       for(int i=0;i<nums.length/2;i++){
+//         nums[2*i]=plus[i];
+//         nums[(2*i)+1]=minus[i];
+//       }
 
-return nums;
+// return nums;
     
-    
+//   OPTIMAL SOMEWHAT :: 
+
+int even=0; int odd=1;
+int ans[]=new int[nums.length];
+for(int i=0;i<nums.length;i++){
+    if(nums[i]>0){
+        ans[even]=nums[i];
+        even+=2;
+    }
+    else{
+        ans[odd]=nums[i];
+        odd+=2;
+    }
+}
+
+
+    return ans;
     }
 }
