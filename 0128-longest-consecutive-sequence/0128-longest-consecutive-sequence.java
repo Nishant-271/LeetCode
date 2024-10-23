e9d4efd4b7b2f1d3c24d0022c512514857c6f26d
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-10-23 13:56:04 +0530

    Time: 15 ms (96.1%), Space: 56.2 MB (96.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,19 +1,54 @@
 class Solution {
+   
+  
+//  BRUTE FORCE :: TC : O(N*N) ::::::
+    // public int longestConsecutive(int[] nums) {
+//   int longest=0;
+  
+//   for(int i=0;i<nums.length;i++){
+//     int count=1;
+//     int x=nums[i];
+//     while(search(nums,x+1)==true){
+//         x=x+1;
+//         count++;
+//     }
+//      longest=Math.max(longest,count);     
+//   }
+
+// return longest;
+//     }
+// public static boolean search(int nums[],int number){
+//     for(int i=0;i<nums.length;i++){
+//         if(nums[i]==number){
+//             return true;
+//         }
+//     }
+// return false;
+
+// }
+
+//  BETTER SOLUTION :::::
+
     public int longestConsecutive(int[] nums) {
-       Arrays.sort(nums);
-        if(nums.length==0) return 0;
-        int maxlength=0;
-        int count=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i+1]-nums[i]==1){
-                count++; 
-            }
-            else if(nums[i]==nums[i+1]) continue;
-            else{
-                count=0;
-            }
-            maxlength=Math.max(maxlength,count);
-        }
-        return maxlength+1;
+        Arrays.sort(nums);
+   int last_smaller=Integer.MIN_VALUE;
+   int currlength=0;
+   int maxlength=0;
+   for(int i=0;i<nums.length;i++){
+    if(nums[i]-1==last_smaller){
+        currlength++;
+        last_smaller=nums[i];
+    }
+    // else if(nums[i]==last_smaller){
+    //     continue;
+    // }
+    else if(last_smaller!=nums[i]){
+        currlength=1;
+        last_smaller=nums[i];
+    }
+    maxlength=Math.max(maxlength,currlength);
+   }
+
+return maxlength;
     }
 }
