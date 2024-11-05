15839a93435a3d63a4f35c3fff719bafae407bbf
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-05 15:20:37 +0530

    Time: 12 ms (35.56%), Space: 45.2 MB (12.5%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -4,9 +4,8 @@ class Solution {
     
     for(int i=0;i<s.length();i+=2){
     String s2=s.substring(i,i+2);
-     if(s2.equals("00") || s2.equals("11")) continue;
-    else count++; 
-    }
-    return count;
+  count += (s2.equals("00") || s2.equals("11")) ? 0 : 1;
+
+        }    return count;
     }
 }
