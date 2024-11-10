52e6d74c4c1c458625a1eb5f930ed5ec26f6a1c2
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-10 12:59:17 +0530

    Time: 11 ms (5.05%), Space: 42.2 MB (41.37%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.java
+++ 3636-check-balanced-string/3636-check-balanced-string.java
@@ -3,10 +3,11 @@ class Solution {
         // BRUTE FORCE ::
      int odsum=0,evsum=0;
       for(int i=0;i<s.length();i++){
+        int num=Character.getNumericValue(s.charAt(i));
          if(i%2==0){
-         evsum+=Integer.parseInt(String.valueOf(s.charAt(i)));  }
+         evsum+=num;  }
       else{
-         odsum+=Integer.parseInt(String.valueOf(s.charAt(i)));
+         odsum+=num;
         }    
     }
     System.out.println(evsum + " " + odsum);
