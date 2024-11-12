834f94efe36bd3816802e92d38476107e1b612ab
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-12 20:42:44 +0530

    Time: 14 ms (98.86%), Space: 56.8 MB (84.32%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,54 +1,58 @@
 class Solution {
-   
-  
-//  BRUTE FORCE :: TC : O(N*N) ::::::
-    // public int longestConsecutive(int[] nums) {
-//   int longest=0;
-  
-//   for(int i=0;i<nums.length;i++){
-//     int count=1;
-//     int x=nums[i];
-//     while(search(nums,x+1)==true){
-//         x=x+1;
-//         count++;
-//     }
-//      longest=Math.max(longest,count);     
-//   }
-
-// return longest;
-//     }
-// public static boolean search(int nums[],int number){
+    
+    // BRUTE FORCE :: O(N*N) 
+//     public int longestConsecutive(int[] nums) {
+      
+//         int max=0;
 //     for(int i=0;i<nums.length;i++){
-//         if(nums[i]==number){
-//             return true;
+//         int count=1;
+//         int x=nums[i];
+//         while(search(nums,x+1)){
+//             x=x+1;
+//             count++;
 //         }
-//     }
-// return false;
-
+//         max=Math.max(max,count);
+//     }  
+//   return max;
 // }
 
-//  BETTER SOLUTION :::::
 
+// public static boolean search(int nums[],int val){
+//     for(int i:nums){
+//         if(i==val) return true;
+//     }
+//     return false;
+// }
+
+
+
+
+//  BETTER SOLUTION ::
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-   int last_smaller=Integer.MIN_VALUE;
-   int currlength=0;
-   int maxlength=0;
-   for(int i=0;i<nums.length;i++){
-    if(nums[i]-1==last_smaller){
-        currlength++;
-        last_smaller=nums[i];
+    Arrays.sort(nums);
+    int max=0; int count=0;int prev=Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]-1==prev){
+            count++;
+            prev=nums[i];
+        }
+        else if(nums[i]==prev) continue;
+        else if(nums[i]!=prev){
+            count=1;
+            prev=nums[i];
+        }
+        max=Math.max(count,max);
     }
-    else if(nums[i]==last_smaller){
-        continue;
+return max;
     }
-    else if(last_smaller!=nums[i]){
-        currlength=1;
-        last_smaller=nums[i];
-    }
-    maxlength=Math.max(maxlength,currlength);
-   }
 
-return maxlength;
-    }
+
+
+
+
+
+
+
+
+
 }
