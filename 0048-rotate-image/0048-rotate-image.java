373c6ca725e02ff51758f7e604a653ea80246f58
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-19 11:26:13 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.23%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+//  BRUTE FORCE WE HAVE TO UPDATE THE ORIGINAL ONE ::      
+          
+  int n=matrix.length;
+  int ans[][]=new int[n][n];
+
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        ans[j][n-1-i]=matrix[i][j];
+    }
+  }
+
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+    matrix[i][j]=ans[i][j]   ;   
+    }
+  }
+
+  
+
+//  BETTER ONE FIRST WE WILL TRANSPOSE BY USING THE UPPER HALF AFTER THAT REVERSE EACH SECTION :
+
+
+
+
+    }
+}
