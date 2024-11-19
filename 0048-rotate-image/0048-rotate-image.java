ded24b36dd23c4fc26eef5fef6fa72774acce425
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-19 12:03:54 +0530

    Time: 0 ms (100%), Space: 42 MB (78.07%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,25 +1,45 @@
 class Solution {
-    public void rotate(int[][] matrix) {
+    public void rotate(int[][] mat) {
 //  BRUTE FORCE WE HAVE TO UPDATE THE ORIGINAL ONE ::      
           
-  int n=matrix.length;
-  int ans[][]=new int[n][n];
+//   int n=matrix.length;
+//   int ans[][]=new int[n][n];
 
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-        ans[j][n-1-i]=matrix[i][j];
-    }
-  }
+//   for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//         ans[j][n-1-i]=matrix[i][j];
+//     }
+//   }
 
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-    matrix[i][j]=ans[i][j]   ;   
-    }
-  }
+//   for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//     matrix[i][j]=ans[i][j]   ;   
+//     }
+//   }
 
   
 
 //  BETTER ONE FIRST WE WILL TRANSPOSE BY USING THE UPPER HALF AFTER THAT REVERSE EACH SECTION :
+
+    int n=mat.length;
+
+    //  transpose ::
+    for(int i=0;i<=n-2;i++){
+        for(int j=i+1;j<n;j++){
+            int temp=mat[i][j];
+            mat[i][j]=mat[j][i];
+            mat[j][i]=temp;
+        }
+    }
+
+//   reverse ::
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            int temp=mat[i][j];
+            mat[i][j]=mat[i][n-1-j];
+            mat[i][n-1-j]=temp;
+        }
+    }
 
 
 
