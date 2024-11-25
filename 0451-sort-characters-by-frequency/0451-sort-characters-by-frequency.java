8b051d9db966a130b58e0c6abb7fc578dd0adb24
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-25 15:56:31 +0530

    Time: 16 ms (44.76%), Space: 45.6 MB (56.17%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,19 +1,55 @@
 class Solution {
+  class pair{
+    char ch;
+    int freq;
+     pair(char ch,int freq){
+        this.ch=ch;
+        this.freq=freq;
+     }
+  }
+
+
     public String frequencySort(String s) {
- TreeMap<Character,Integer> map=new TreeMap<>();
- for(char c:s.toCharArray()){
-    map.put(c,map.getOrDefault(c,0)+1);
- }
+//  BRUTE FORCE ::
+//  Map<Character,Integer> map=new HashMap<>();
+//  for(char c:s.toCharArray()){
+//     map.put(c,map.getOrDefault(c,0)+1);
+//  }
   
-  StringBuilder ans=new StringBuilder();
-  for(int count=s.length();count>0;count--){
-    for(char j:map.keySet()){
-        if(map.get(j)==count){
-            ans.append(String.valueOf(j).repeat(count));
-        }
+//   StringBuilder ans=new StringBuilder();
+//   for(int count=s.length();count>0;count--){
+//     for(char j:map.keySet()){
+//         if(map.get(j)==count){
+//             ans.append(String.valueOf(j).repeat(count));
+//         }
+//     }
+//   }
+  
+//   return ans.toString();
+   
+//    OPTIMAL : the above class is defined for optimal one 
+    HashMap<Character,Integer> map=new HashMap<>();
+    for(char ch : s.toCharArray()){
+        map.put(ch,map.getOrDefault(ch,0)+1);
     }
+ 
+ PriorityQueue<pair> pq=new PriorityQueue<>((a,b)->b.freq-a.freq);
+ for(char i:map.keySet()){
+    pq.offer(new pair(i,map.get(i)));
+ }
+ 
+ StringBuilder ans=new StringBuilder();
+while(!pq.isEmpty()){
+    pair p=pq.remove();
+    char c=p.ch;
+    int val=p.freq;
+    ans.append(String.valueOf(c).repeat(val));
+}
+
+return ans.toString();
+
+ 
   }
-  
-  return ans.toString();
-  }
+
+
 }
