ad9b5779ade2a4601cc8287049e59cc76ba3674c
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-26 11:29:07 +0530

    Time: 2 ms (99.36%), Space: 48.3 MB (12.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -26,16 +26,55 @@ class Solution {
 
 
     //  BETTER APPROCH 
-      List<Integer> arr=new ArrayList<>();
-      HashMap<Integer,Integer> map=new HashMap<>();
-      int max=(int)nums.length/3+1;
-      for(int i:nums){
-        map.put(i,map.getOrDefault(i,0)+1);
-        if(map.get(i)==max){
-            arr.add(i);
-        }
-        if(arr.size()==2) break;
-      }
+    //   List<Integer> arr=new ArrayList<>();
+    //   HashMap<Integer,Integer> map=new HashMap<>();
+    //   int max=(int)nums.length/3+1;
+    //   for(int i:nums){
+    //     map.put(i,map.getOrDefault(i,0)+1);
+    //     if(map.get(i)==max){
+    //         arr.add(i);
+    //     }
+    //     if(arr.size()==2) break;
+    //   }
+
+//  OPTIMAL SOLUTION : BOyer's moore's voting algorithm ::
+
+List<Integer> arr=new ArrayList<>();
+int count1=0,count2=0,el1=Integer.MIN_VALUE,el2=Integer.MIN_VALUE;
+for(int i:nums){
+    if(count1==0 && i!=el2){
+        count1=1;
+        el1=i;
+    }
+    else if(count2==0 && i!=el1){
+        count2=1;
+        el2=i;
+    }
+    else if(el1==i) count1++;
+    else if(el2==i) count2++;
+
+    else{
+        count1--;
+        count2--;
+    }
+}
+ 
+count1=0; count2=0;
+for(int i:nums){
+    if(i==el1) count1++;
+    if(i==el2) count2++;
+}
+if(count1>nums.length/3)
+arr.add(el1);
+if(count2>nums.length/3)
+arr.add(el2);
+
+
+
+
+
+
+
 return arr;
     }
 }
