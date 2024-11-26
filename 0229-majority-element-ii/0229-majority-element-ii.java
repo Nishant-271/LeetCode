c79db6d51150c83b8ff2dd0bcc6b80b5f61f5fad
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-11-26 10:50:50 +0530

    Time: 16 ms (9.71%), Space: 47.5 MB (72.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,23 +2,23 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
      
     //  BRUTE FORCE :
-  List<Integer> ll=new ArrayList<>();
-    for(int i=0;i<nums.length;i++){
-      if(ll.size()==0 || ll.get(0)!=nums[i]){
-           int count=0;
-           for(int j=0;j<nums.length;j++){
-            if(nums[j]==nums[i]){
-                count++;
-            }
-           }
-           if(count>nums.length/3){
-            ll.add(nums[i]);
-           }
-        if(ll.size()==2) break;
-      }
-    }
+//   List<Integer> ll=new ArrayList<>();
+//     for(int i=0;i<nums.length;i++){
+//       if(ll.size()==0 || ll.get(0)!=nums[i]){
+//            int count=0;
+//            for(int j=0;j<nums.length;j++){
+//             if(nums[j]==nums[i]){
+//                 count++;
+//             }
+//            }
+//            if(count>nums.length/3){
+//             ll.add(nums[i]);
+//            }
+//         if(ll.size()==2) break;
+//       }
+//     }
 
-return ll;
+// return ll;
 
 
 
@@ -26,17 +26,16 @@ return ll;
 
 
     //  BETTER APPROCH 
-//       List<Integer> arr=new ArrayList<>();
-//       HashMap<Integer,Integer> map=new HashMap<>();
-//       for(int i:nums){
-//         map.put(i,map.getOrDefault(i,0)+1);
-//       }
-//  for(int i:map.keySet()){
-//     if(map.get(i)>nums.length/3){
-//         arr.add(i);
-//     }
-//  }
-
-// return arr;
+      List<Integer> arr=new ArrayList<>();
+      HashMap<Integer,Integer> map=new HashMap<>();
+      int max=(int)nums.length/3+1;
+      for(int i:nums){
+        map.put(i,map.getOrDefault(i,0)+1);
+        if(map.get(i)==max){
+            arr.add(i);
+        }
+        if(arr.size()==2) break;
+      }
+return arr;
     }
 }
