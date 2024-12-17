0fd8fc3cc8efb7e202c7cd746177e8d6645959f5
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-17 20:59:38 +0530

    Time: 1 ms (37.18%), Space: 40.6 MB (76.67%) - LeetHub

--- 1289-day-of-the-week/1289-day-of-the-week.java
+++ 1289-day-of-the-week/1289-day-of-the-week.java
@@ -1,10 +1,19 @@
+// import java.time.LocalDate;
+
+// class Solution {
+//     public String dayOfTheWeek(int day, int month, int year) {
+//         String[] dayNames = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+//         LocalDate date = LocalDate.of(year, month, day);
+//         return dayNames[date.getDayOfWeek().getValue() - 1];
+//     }
+// }
+
 import java.time.LocalDate;
 
-class Solution {
-    public String dayOfTheWeek(int day, int month, int year) {
-        String[] dayNames = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        LocalDate date = LocalDate.of(year, month, day);
-        return dayNames[date.getDayOfWeek().getValue() - 1];
+class Solution{
+    public String dayOfTheWeek(int day,int month,int year){
+        String[] dayindex={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        LocalDate date=LocalDate.of(year,month,day);
+        return dayindex[date.getDayOfWeek().getValue()-1];
     }
 }
-
