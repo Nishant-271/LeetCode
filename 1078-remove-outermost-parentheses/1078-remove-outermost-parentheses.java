a8030562f58dfd1dfe1b6da6eaa39c1a39bd288f
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-19 19:18:26 +0530

    Time: 7 ms (38.96%), Space: 42.2 MB (54.03%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -3,21 +3,16 @@ class Solution {
         StringBuilder ans=new StringBuilder();
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
-              char ch=s.charAt(i);
-              if(ch=='('){
-                if(st.size()>0){
-                    ans.append(ch);
-                }
+            char ch=s.charAt(i);
+            if(ch=='('){
+                if(st.size()>0) ans.append(ch);
                 st.push(ch);
-              }
-              else{
+            }
+            else{
                 st.pop();
-                if(st.size()>0){
-                    ans.append(ch);
-                }
-              }
-        
+                if(st.size()>0) ans.append(ch);
+            }
         }
-        return ans.toString();
+    return ans.toString();
     }
 }
