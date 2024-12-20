45c99a98b2e1dabf720f30c60f470966e60adca7
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-20 21:26:53 +0530

    Time: 16 ms (46.3%), Space: 45.8 MB (41.19%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,19 +1,29 @@
 class Solution {
-    public String frequencySort(String s) {
-        // BRUTE FORCE 
-      Map<Character,Integer> map=new HashMap<>();
-      for(char c:s.toCharArray()){
-        map.put(c,map.getOrDefault(c,0)+1);
-      }
-
-      StringBuilder ans=new StringBuilder();
-      for(int count=s.length();count>0;count--){
-        for(char j:map.keySet()){
-            if(map.get(j)==count){
-                ans.append(String.valueOf(j).repeat(count));
-            }
-        }
-      }
-return ans.toString();
+   class pair{
+    char ch; int freq;
+    pair(char ch,int freq){
+        this.ch=ch;
+        this.freq=freq;
     }
+   }
+
+    public String frequencySort(String s) {
+        Map<Character,Integer> map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+    PriorityQueue<pair> pq=new PriorityQueue<>((a,b)->b.freq-a.freq);
+    for(char j:map.keySet()){
+        pq.offer(new pair(j,map.get(j)));
+    }
+
+    StringBuilder ans=new StringBuilder();
+    while(!pq.isEmpty()){
+        pair p=pq.remove();
+        char c=p.ch;
+        int count=p.freq;
+      ans.append(String.valueOf(c).repeat(count));
+    }
+  return ans.toString();
+  }
 }
