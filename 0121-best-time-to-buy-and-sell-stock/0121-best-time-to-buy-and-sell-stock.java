9fd2a2204e20886022dafaa078959e5969673557
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-26 13:19:44 +0530

    Time: 70 ms (2.57%), Space: 57 MB (99.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -12,13 +12,39 @@ class Solution {
     // return max;
 
 // OPTIMAL ONE way  ::
- int buy=prices[0];
- int profit=0;
- for(int i=1;i<prices.length;i++){
-    if(prices[i]<buy) buy=prices[i];
-    profit=Math.max(profit,prices[i]-buy); 
- }
-   return profit;
+//  int buy=prices[0];
+//  int profit=0;
+//  for(int i=1;i<prices.length;i++){
+//     if(prices[i]<buy) buy=prices[i];
+//     profit=Math.max(profit,prices[i]-buy); 
+//  }
+//    return profit;
 
+// OPTIMAL SECOND WAY ::
+int n=prices.length;
+int max=0;
+int left[]=new int[n];
+int right[]=new int[n];
+left[0]=prices[0];
+right[n-1]=prices[n-1];
+
+//  for buy maintain the array of left 
+for(int i=1;i<n;i++){
+    left[i]=Math.min(left[i-1],prices[i]);
+}
+System.out.print(Arrays.toString(left));
+//  for sell maintain the array of right 
+for(int i=n-2;i>0;i--){
+    right[i]=Math.max(right[i+1],prices[i]);
+}
+System.out.print(Arrays.toString(right));
+
+
+// FINDING THE MAX PROFIT 
+
+for(int i=0;i<n;i++){
+    max=Math.max(max,right[i]-left[i]);
+}
+return max;
     }
 }
