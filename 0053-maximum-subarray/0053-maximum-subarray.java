9e37359d54a426afd74bd07e688db0eec67a5259
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-26 14:42:04 +0530

    Time: 1 ms (99.4%), Space: 57 MB (62.32%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,32 +1,46 @@
 class Solution {
     public int maxSubArray(int[] nums) {
     //   BRUTE FORCE ::  TC-O(N*N)
-      
-//         int maxsum=Integer.MIN_VALUE;
-//         for(int i=0;i<nums.length;i++){
-//               int sum=0;
-//               for(int j=i;j<nums.length;j++){
-//                 sum+=nums[j];
-//                 maxsum=Math.max(sum,maxsum);
-//               }
-//         }
-// return maxsum;
-    
-    
-    
-    // OPTIMAL  SOLUTION : KADAN'S ALGORITHM ::
-    int max=Integer.MIN_VALUE;
-    int sum=0;
-    for(int i=0;i<nums.length;i++){
-        sum+=nums[i];
-         if(sum>max){
-            max=sum;
-         }
+//    int max=Integer.MIN_VALUE ;
+//    for(int i=0;i<nums.length;i++){
+//     int sum=0;
+//     for(int j=i;j<nums.length;j++){
+//         sum+=nums[j];
+//         max=Math.max(max,sum);
+//     }
+//    }
+//  return max;
 
-        if(sum<0){
-            sum=0;
-        }
+    
+    
+//     // OPTIMAL  SOLUTION : KADAN'S ALGORITHM ::
+  int max=Integer.MIN_VALUE;
+  int sum=0;
+  for(int i=0;i<nums.length;i++){
+    sum+=nums[i];
+    if(sum>max){
+        max=sum;
     }
-    return max;
+    if(sum<0)sum=0;
+  }
+
+
+return max;
+
+
+
+
+
+
+// //  FOLLOW UP QUESTION :: Agar mujhe maximum sum vali subarrays ko bhi print krna hai 
+// // toh main do pointers ko maintain krunga ek toh starting vala and ending vala aur 
+// // vhi subarray hogi meri 
+// // to solve this follow up question solve the problem on geekforgeek striver sheet.
+ 
+
+
+
+
+
     }
 }
