4d2b7dbf8ad388a5e86964ceb36caecf861510d7
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-26 15:45:49 +0530

    Time: 271 ms (7.39%), Space: 45.4 MB (8.13%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+    // BRUTE FORCE WAY 
+ int max=Integer.MIN_VALUE;
+ for(int i=0;i<nums.length;i++){
+    int mul=1;
+    for(int j=i;j<nums.length;j++){
+       mul*=nums[j];
+       max=Math.max(max,mul);
+    }
+ }
+return max;
+
+        }
+}
