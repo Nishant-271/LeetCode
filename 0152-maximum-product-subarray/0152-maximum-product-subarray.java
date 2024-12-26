c8bcb352ba08f53eddaaed715e5a13cb0db29979
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-26 16:06:32 +0530

    Time: 1 ms (88.52%), Space: 44.6 MB (90.55%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,15 +1,31 @@
 class Solution {
     public int maxProduct(int[] nums) {
-    // BRUTE FORCE WAY 
- int max=Integer.MIN_VALUE;
- for(int i=0;i<nums.length;i++){
-    int mul=1;
-    for(int j=i;j<nums.length;j++){
-       mul*=nums[j];
-       max=Math.max(max,mul);
-    }
- }
-return max;
+    // BRUTE FORCE WAY O(N*N) 
 
+//  int max=Integer.MIN_VALUE;
+//  for(int i=0;i<nums.length;i++){
+//     int mul=1;
+//     for(int j=i;j<nums.length;j++){
+//        mul*=nums[j];
+//        max=Math.max(max,mul);
+//     }
+//  }
+// return max;
+
+//   OPTIMAL ONE ::
+
+  int prefix=1,suffix=1;
+  int max=Integer.MIN_VALUE;
+  for(int i=0;i<nums.length;i++){
+   if(prefix==0) prefix=1;
+   if(suffix==0) suffix=1;
+  
+   prefix=prefix*nums[i];
+   suffix=suffix*nums[nums.length-i-1];
+   max=Math.max(max,Math.max(prefix,suffix));
+  
+  }
+
+return max;
         }
 }
