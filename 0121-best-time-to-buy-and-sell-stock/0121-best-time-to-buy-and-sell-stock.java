ace1dfbb894e694d2ac1e24fb2cb3241558daa26
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-12-26 13:12:09 +0530

    Time: 2 ms (76.92%), Space: 62.1 MB (15.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int max=0;
-        int left[]=new int[n];
-        int right[]=new int[n];
-         left[0]=prices[0];
-         right[n-1]=prices[n-1];
-      
-    //   FOR BUYING HAVING THE SMALLEST VALUE 
-       for(int i=1;i<n;i++){
-        left[i]=Math.min(left[i-1],prices[i]);
-       }
-       // FOR SELLING HAVING THE LARGEST VALUE 
- for(int i=n-2;i>=0;i--){
-    right[i]=Math.max(right[i+1],prices[i]);
+  
+//   BRUTE FORCE :: TLE CODE 
+  
+    // int max=0;
+    // for(int i=0;i<prices.length;i++){
+    //     for(int j=i+1;j<prices.length;j++){
+    //         max=Math.max(max,prices[j]-prices[i]);
+    //     }
+    // }
+    // return max;
+
+// OPTIMAL ONE way  ::
+ int buy=prices[0];
+ int profit=0;
+ for(int i=1;i<prices.length;i++){
+    if(prices[i]<buy) buy=prices[i];
+    profit=Math.max(profit,prices[i]-buy); 
  }
-   
-//  HAVING THE MAXIMUM PROFIT
-  for(int i=0;i<n;i++){
-    max=Math.max(max,right[i]-left[i]);
-  }
-return max;
+   return profit;
+
     }
 }
