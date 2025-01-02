cddf48e5d066212f8bc6be53998e8f5644d3271e
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-02 22:36:15 +0530

    Time: 7 ms (5.69%), Space: 42.6 MB (6.96%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,9 +1,21 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
     //    FIRST APPROCH ::::::::::
- HashMap<Integer,Integer> map=new HashMap<>();
- for(int i:arr) map.put(i,map.getOrDefault(i,0)+1);
-HashSet<Integer> st=new HashSet<>(map.values());
-return st.size()==map.size();
+
+//  HashMap<Integer,Integer> map=new HashMap<>();
+//  for(int i:arr) map.put(i,map.getOrDefault(i,0)+1);
+// HashSet<Integer> st=new HashSet<>(map.values());
+// return st.size()==map.size();
+
+   // SECOND APPROCH     :::::::::::::::::
+
+int main[]=new int[2001];
+for(int i:arr) main[i+1000]++;
+Arrays.sort(main);
+for(int i=1;i<2001;i++){
+    if(main[i]!=0 && main[i]==main[i-1]) return false;
+}
+
+return true;
     }
 }
