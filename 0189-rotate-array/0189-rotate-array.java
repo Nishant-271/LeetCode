f0d1ca64fcae810b0bd959d4daddc6224f968ab9
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-03 11:01:30 +0530

    Time: 0 ms (100%), Space: 57.4 MB (43.09%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,19 +2,33 @@ class Solution {
     public void rotate(int[] nums, int k) {
         // BRUTE FORCE ::::
 // BY MAKING THE TEMPORARY ARRAY 
-k=k%nums.length;
-int temp[]=new int[k];
-for(int i=0;i<k;i++){
-    temp[i]=nums[nums.length-k+i];
-}
+// k=k%nums.length;
+// int temp[]=new int[k];
+// for(int i=0;i<k;i++){
+// temp[i]=nums[nums.length-k+i];
+// }
+//   for(int i=nums.length-1;i>=k;i--){
+//         nums[i]=nums[i-k];
+//     }
+//   for(int i=0;i<k;i++){
+//     nums[i]=temp[i];
+//   }   
+ k=k%nums.length;
+ reverse(0,nums.length-1,nums);
+ reverse(0,k-1,nums);
+ reverse(k,nums.length-1,nums);
 
 
-for(int i=nums.length-1;i>=k;i--){
-    nums[i]=nums[i-k];
-}
 
-for(int i=0;i<k;i++){
-    nums[i]=temp[i];
-}
     }
+
+public static void reverse(int start,int end,int nums[]){
+    while(start<end){
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+    start++;end--;
+    }
+}
+
 }
