cfebd8518d5d9b8c7d85b914fafb8f74ebf6f17b
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-11 12:33:13 +0530

    Time: 1 ms (99.91%), Space: 44.7 MB (25.6%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,22 +1,39 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-   List<Integer> ll=new ArrayList<>();
-   for(ListNode node:lists){
-    while(node!=null){
-      ll.add(node.val);
-      node=node.next;
-   }
-}
+   int k=lists.length;
+ if(k==0) return null;
+ 
+ return partitionMerge(0,k-1,lists);  
 
- Collections.sort(ll);
-
- ListNode head=new ListNode(-1);
- ListNode temp=head;
- for(int val:ll){
-    temp.next=new ListNode(val);
-    temp=temp.next;
  }
 
-return head.next;
+ public static ListNode partitionMerge(int start,int end,ListNode[] lists){
+    if(start>end) return null;
+   if(start==end) return lists[start];
+
+  int mid=start+(end-start)/2;
+    ListNode L1=partitionMerge(start,mid,lists);
+    ListNode L2=partitionMerge(mid+1,end,lists);
+
+return merge_two(L1,L2);
+
+ }
+
+public static ListNode merge_two(ListNode l1,ListNode l2){
+   if(l1==null) return l2;
+   if(l2==null) return l1;
+   
+    if(l1.val<l2.val){
+        l1.next=merge_two(l1.next,l2);
+        return l1;
+    }else{
+        l2.next=merge_two(l2.next,l1);
+        return l2;
     }
+
+
 }
+
+
+
+  }
