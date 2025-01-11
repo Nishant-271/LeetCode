0ea8271f4121229eb031bd94f0030a04a0e1bab8
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-11 10:22:50 +0530

    Time: 0 ms (100%), Space: 44.8 MB (73.48%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null) return null;
+        if(head.next==null) return new TreeNode(head.val);
+    ListNode slow=head,fast=head,slow_prev=null;
+    while(fast!=null && fast.next!=null){
+        slow_prev=slow;
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    TreeNode root=new TreeNode(slow.val);
+     slow_prev.next=null; // from mid we have to break the link towards left
+    //  hand side.
+    root.left=sortedListToBST(head);
+    root.right=sortedListToBST(slow.next);
+ 
+return root;
+    }
+}
