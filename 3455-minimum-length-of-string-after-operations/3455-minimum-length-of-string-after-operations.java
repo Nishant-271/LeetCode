57dfdb792c9e78dbfca83dad8da437738d0f6ba1
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-13 22:22:59 +0530

    Time: 10 ms (82.12%), Space: 46.3 MB (53.64%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,23 +1,12 @@
 class Solution {
     public int minimumLength(String s) {
-        Map<Character,Integer> map=new HashMap<>();
-        for(char ch:s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-     int j=0;
-     for(char ch:map.keySet()){
-        int val=map.get(ch);
-        if(val<3){
-            j=j+map.get(ch);
-         }
-      else{
-        if(map.get(ch)%2!=0){
-            j=j+1;
-        }else{
-            j=j+2;
-        }
-      }
-     }
-return j;
+    int length=0;    int freq[]=new int[26];
+    for(char ch:s.toCharArray())   freq[ch-'a']++;
+   for(int f:freq){
+        if(f==0) continue;
+        if(f%2==0) length+=2; 
+        else  length+=1;    }
+    return length;
+    
     }
 } 
