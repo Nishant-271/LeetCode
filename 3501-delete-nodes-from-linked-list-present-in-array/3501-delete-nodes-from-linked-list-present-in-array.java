5ca0faf3fdce5d283f519e49a7cc51c46ec4be32
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-20 15:53:09 +0530

    Time: 19 ms (90.45%), Space: 64.6 MB (54.65%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -1,29 +1,21 @@
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
+   
+   HashSet<Integer> st=new HashSet<>();
+   for(int i:nums) st.add(i);
+   ListNode dummy=new ListNode(0);
+   dummy.next=head;
 
-  HashSet<Integer> st=new HashSet<>();
-  for(int i:nums) st.add(i);
-    while(st.contains(head.val)){
-        //  ListNode temp=head;
-        head=head.next;
-        // temp.next=null;  // deleting the node
-        //  completeling not left in a hanging position 
-
-    }
-   ListNode curr=head;
+   ListNode curr=dummy;
    while(curr.next!=null){
-   if(st.contains(curr.next.val)){
-    //  ListNode temp2=curr.next;
-    curr.next=curr.next.next;
-    // temp2.next=null;
+    if(st.contains(curr.next.val)){
+        curr.next=curr.next.next;
+    }
+    else{
+        curr=curr.next;
+    }
    }
-   else{
-    curr=curr.next;
-   }
-
-   }
-
-return head;
+return dummy.next;
 
     }
 }
