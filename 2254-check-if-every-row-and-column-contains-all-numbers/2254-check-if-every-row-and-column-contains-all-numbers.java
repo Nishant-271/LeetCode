67288314216c2d97926a5007687936a78844f8e6
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-20 16:16:28 +0530

    Time: 3 ms (98.26%), Space: 45.6 MB (35.8%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,17 +1,37 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
-        HashSet<Integer> st=new HashSet<>();
-       int n=matrix.length;   for(int i=1;i<=n;i++)  st.add(i);
-        for(int i=0;i<n;i++){
-            HashSet<Integer> row=new HashSet<>();
-            HashSet<Integer> col=new HashSet<>();
-            for(int j=0;j<n;j++){
-               row.add(matrix[i][j]); 
-               col.add(matrix[j][i]); }
-            if(!st.equals(row)|| !st.equals(col)){
-                return false;
-            }
-        }
+//  BRUTE FORCE :::::
+
+//         HashSet<Integer> st=new HashSet<>();
+//        int n=matrix.length;   for(int i=1;i<=n;i++)  st.add(i);
+//         for(int i=0;i<n;i++){
+//             HashSet<Integer> row=new HashSet<>();
+//             HashSet<Integer> col=new HashSet<>();
+//             for(int j=0;j<n;j++){
+//                row.add(matrix[i][j]); 
+//                col.add(matrix[j][i]); }
+//             if(!st.equals(row)|| !st.equals(col)){
+//                 return false;
+//             }
+//         }
+
+// return true;
+
+//   ANOTHER WAY :::::::::
+  int n=matrix.length;
+  for(int i=0;i<n;i++){
+    int row[]=new int[n+1], col[]=new int[n+1];
+  for(int j=0;j<n;j++){
+    row[matrix[i][j]]++;
+    col[matrix[j][i]]++;
+  }
+ for(int k=1;k<=n;k++){
+    if(row[k]>1  || col[k]>1) return false;
+  }
+  }
+
+
+
 
 return true;
     }
