2fb629c0b2c095b595989189c903a3a6188abe4a
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-23 12:48:18 +0530

    Time: 10 ms (17.94%), Space: 45.7 MB (42.38%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,39 @@
 class Solution {
     public boolean canFinish(int V, int[][] edges) {
         HashMap<Integer,List<Integer>> map=new HashMap<>();
-    int indegree[]=new int[V];
-        for(int []edge:edges){
-           int end=edge[0];
-           int start=edge[1];
-        if(!map.containsKey(start)){
-            map.put(start,new ArrayList<>());
-        }
-        map.get(start).add(end);
-        indegree[end]++;
-     }
+        int indegree[]=new int[V];
+        for(int [] edge:edges){
+            int end=edge[0];
+            int start=edge[1];
 
-    Queue<Integer> q=new LinkedList<>();
-  int count=0;
- for(int u=0;u<V;u++){
-    if(indegree[u]==0){
-        q.add(u);
-        count++;
+            if(!map.containsKey(start)){
+                map.put(start,new ArrayList<>());
+            }
+            map.get(start).add(end);
+            indegree[end]++;
+        }
+       int count=0;
+       Queue<Integer> q=new LinkedList<>();
+       for(int u=0;u<V;u++){
+        if(indegree[u]==0){
+            q.add(u);
+            count++;
+        }
+       }
+ 
+ while(!q.isEmpty()){
+    int u=q.remove();
+    if(map.containsKey(u)){
+    for(int nbr:map.get(u)){
+        indegree[nbr]--;
+        if(indegree[nbr]==0){
+            q.add(nbr);
+            count++;
+        }
+    }
     }
  }
 
-  
-while(!q.isEmpty()){
-    int u=q.remove();
-    if(map.containsKey(u)){
-    for(int v:map.get(u)){
-        indegree[v]--;
-        if(indegree[v]==0){
-            q.add(v);
-            count++;
-        }
-    }}
-}
- 
- if(count==V) return true;
- return false;
- 
+return count==V;
     }
-
 }
