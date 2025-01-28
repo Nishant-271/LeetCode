9cf910634d1ecf0240a3a9b984f343b2d3c9a5a2
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-01-28 07:16:48 +0530

    Time: 0 ms (100%), Space: 44.9 MB (63.81%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -2,10 +2,42 @@ class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
        if(root1==null) return root2;
        if(root2==null) return root1;
-        TreeNode newroot=new TreeNode(root1.val+root2.val);
-        newroot.left=mergeTrees(root1.left,root2.left);
-        newroot.right=mergeTrees(root1.right,root2.right);
-return newroot;
+
+       TreeNode newroot=new TreeNode(root1.val+root2.val);
+       merge(newroot,root1,root2);
+
+       return newroot;
     }
+  public static void merge(TreeNode root,TreeNode root1,TreeNode root2){
+  
+  if(root1.left!=null || root2.left!=null){
+     if(root1.left==null){
+        root.left=root2.left;
+     }
+     else if(root2.left==null){
+        root.left=root1.left;
+     }
+   else{
+    TreeNode newLeft=new TreeNode(root1.left.val+root2.left.val);
+    root.left=newLeft;
+    merge(root.left,root1.left,root2.left);
+  }
+  }
+
+  if(root1.right!=null || root2.right!=null){
+     if(root1.right==null){
+        root.right=root2.right;
+     }
+     else if(root2.right==null){
+        root.right=root1.right;
+     }
+   else{
+   
+    TreeNode newRight=new TreeNode(root1.right.val+root2.right.val);
+    root.right=newRight;
+    merge(root.right,root1.right,root2.right);
+  }
+  }
+  }
 
 }
