2929e1b1053e256f4d9723e2e6a2b2238ef4cc67
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2025-02-03 16:00:39 +0530

    Time: 1 ms (93.94%), Space: 42.7 MB (73.06%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -4,24 +4,21 @@ class Solution {
         int dec[]=new int[nums.length];
         Arrays.fill(inc,1);
         Arrays.fill(dec,1);
+          int max_dec=dec[0];   
           int max_inc=inc[0];   
         for(int i=1;i<nums.length;i++){
             if(nums[i]>nums[i-1]){
                 inc[i]=inc[i-1]+1;
               max_inc=Math.max(inc[i],max_inc);
             }
-
-        }
-          int max_dec=dec[0];   
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]<nums[i-1]){
+              if(nums[i]<nums[i-1]){
                 dec[i]=dec[i-1]+1;
               max_dec=Math.max(dec[i],max_dec);
             }
 
         }
-    System.out.print(max_inc+" "+max_dec);
-
+       
+ 
 return Math.max(max_inc,max_dec);
     }
 }
